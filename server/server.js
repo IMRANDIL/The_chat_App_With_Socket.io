a2ac1afee84d5b549723d1940ffca556e1d51fac
IMRANDIL <aliimranadil2@gmail.com>
2022-02-27 11:37:10 +0530

    yh

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const cors = require('cors');
 const socketio = require('socket.io');
 const router = require('./router/router');
 
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users')
+
 
 
 
@@ -29,13 +31,20 @@ const server = http.createServer(app)
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('we have a new connection');
-
     socket.on('join', ({ name, room }, cb) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
 
+        if (error) {
+            return cb(error)
+        }
 
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
 
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
+
+        socket.join(user.room)
+
+        cb()
 
     })
 
